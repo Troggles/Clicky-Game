8472b3dfb87725dc7db1fb92e72cefce9db65daa
Adam Griepentrog <adammiltron@gmail.com>
2018-11-30 22:59:17 -0600

    react - renders app - logic working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from "./components/Card";
+import Wrapper from "./components/wrapper";
+import Score from "./components/Score";
+import pups from "./cards.json";
 import './App.css';
 
 class App extends Component {
+  state = {
+    pups,
+    clickedPuppyIds: [],
+    score: 0, 
+    goal: 8,
+    status: ""
+  };
+
+  shuffleScoreCard = id => {
+    let clickedPuppyIds = this.state.clickedPuppyIds;
+
+    if(clickedPuppyIds.includes(id)) {
+      this.setState({ clickedPuppyIds: [], score: 0, status: "Game Over!"});
+      return;
+    } else {
+      clickedPuppyIds.push(id)
+
+      if(clickedPuppyIds.length === 8) {
+        this.setState({score: 8, status: "You Are A Winner", clickedPuppyIds: [] });
+        console.log("You're Mind is a Steel Trap!");
+        return;
+      }
+
+      this.setState({ pups, clickedPuppyIds, score: clickedPuppyIds.length, status: " "}); 
+
+      for (let i = pups.length -1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [pups[i], pups[j]] = [pups[j], pups[i]];
+      }
+    }
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <header className="App-Header">
+          <h1 className="App-title">The Clickster</h1>
+          <p className="App-Intro">
+            Your Task: Don't click the same image twice  
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Score total={this.state.score}
+                goal={8}
+                status={this.state.status}
+        />
+        <Wrapper>
+          {this.state.pups.map(puppy => (
+            <Card
+            shuffleScoreCard={this.shuffleScoreCard}
+            id={puppy.id}
+            key={puppy.id}
+            image={puppy.image}
+          />
+          ))}
+        </Wrapper>
+        
       </div>
     );
   }
--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Score.css";
-//adding in from documentation for arrow 
-import Arrow from 'react-icons/lib/fa/caret-right';
+
 
 
 
 //score component
 const Score = props => (
     <div className="gameScore">
-        <h3 className="score">Your Score <Arrow />{props.total}</h3>
+        <h3 className="score">Your Score {props.total}</h3>
         <h3 className="status">{props.status}</h3>
     </div>
 );
