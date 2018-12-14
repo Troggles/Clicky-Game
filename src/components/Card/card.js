77e7d5f82cb592b88c2f7d43076b3548233ee8c2
Adam Griepentrog <adammiltron@gmail.com>
2018-12-13 19:08:55 -0600

    fixed error

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -3,7 +3,7 @@ import "./Card.css";
 
 //render images with this component
 const Card = props => (
-    <div className="card -img-conainer hover">
+    <div className="card -img-container hover">
         <img alt={props.name} src={props.image} id={props.id}
             onClick={() => props.shuffleScoreCard(props.id)} className= 'shuffleScore'/>
     </div>
