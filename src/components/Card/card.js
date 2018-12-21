51e8dca6bba33bea9b6c9a1524b826bb457620c9
Adam Griepentrog <adammiltron@gmail.com>
2018-12-20 19:07:21 -0600

    commit

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Card.css";
 
 //render images with this component
-const Card = props => (
+const Card = ({name, image, id, shuffleScoreCard}) => (
     <div className="card -img-container hover">
-        <img alt={props.name} src={props.image} id={props.id}
-            onClick={() => props.shuffleScoreCard(props.id)} className= 'shuffleScore'/>
+        <img alt={name} src={image} id={id}
+            onClick={() => shuffleScoreCard(id)} className= 'shuffleScore'/>
     </div>
 
 );
